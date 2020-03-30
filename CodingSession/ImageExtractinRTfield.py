378d26326649a8414c6607cc01ce82ca8611fec6
yliu19@lenovo.com <yliu19@lenovo.com>
2020-03-30 15:40:28 -0400

    Transform the code into two functions; Add lastmodifiedon >= ? condition for delta; Add upsert into stage and land table

--- CodingSession/ImageExtractinRTfield.py
+++ CodingSession/ImageExtractinRTfield.py
@@ -12,71 +12,31 @@ RTField_list = []
 MD5Hash_list = []
 
 #  Get Salesforce access token and use REST API
-sf_params = {
-    "grant_type": "password",
-    "client_id": "",
-    "client_secret": "",
-    "username": "",
-    "password": ""
-}
+def getaccesstoken():
+    sf_params = {
+        "grant_type": "password",
+        "client_id": "",
+        "client_secret": "",
+        "username": "",
+        "password": ""
+    }
+    r = requests.post("https://login.salesforce.com/services/oauth2/token",
+                      headers={"Content-Type": "application/x-www-form-urlencoded"}, data=sf_params)
+    access_token = r.json().get("access_token")
+    return access_token
 
-r = requests.post("https://login.salesforce.com/services/oauth2/token",
-                  headers={"Content-Type": "application/x-www-form-urlencoded"}, data=sf_params)
-access_token = r.json().get("access_token")
-headers = {
-    'Authorization': 'Bearer ' + access_token,
-}
 
-#  Connect to Carbon database server
-conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      r"Server=;"
-                      "Database=CARBON" + env + ";"
-                      "Trusted_Connection=yes;")
-cursor = conn.cursor()  # connection to get all rich text fields with <img> tags
-
-# Questions from Sophia: tried to use delta load but cannot figure out a better way to do it...
-cursor.execute(r"""
-    select Id, CaseNumber, 'Problem_Recreation__c' as API_Name, cast(Problem_Recreation__c as nvarchar(max)) as RichText from SFCaseLand 
-    where cast(Problem_Recreation__c as nvarchar(max)) is not null and cast(Problem_Recreation__c as nvarchar(max)) like '%</img>%'
-    union
-    select Id, CaseNumber, 'Root_Cause_FA__c', cast(Root_Cause_FA__c as nvarchar(max)) from SFCaseLand
-    where cast(Root_Cause_FA__c as nvarchar(max)) is not null and cast(Root_Cause_FA__c as nvarchar(max)) like '%</img>%'
-    union
-    select Id, CaseNumber, 'Most_Recent_Case_Comment__c', cast(Most_Recent_Case_Comment__c as nvarchar(max)) from SFCaseLand
-    where cast(Most_Recent_Case_Comment__c as nvarchar(max)) is not null and cast(Most_Recent_Case_Comment__c as nvarchar(max)) like '%</img>%'
-    union
-    select Id, CaseNumber, 'HubCase_Parnter_Comments__c', cast(HubCase_Parnter_Comments__c as nvarchar(max)) from SFCaseLand
-    where cast(HubCase_Parnter_Comments__c as nvarchar(max)) is not null and cast(HubCase_Parnter_Comments__c as nvarchar(max)) like '%</img>%'
-    union
-    select Id, CaseNumber, 'Current_Case_Status__c', cast(Current_Case_Status__c as nvarchar(max)) from SFCaseLand
-    where cast(Current_Case_Status__c as nvarchar(max)) is not null and cast(Current_Case_Status__c as nvarchar(max)) like '%</img>%'
-    union
-    select Id, CaseNumber, 'Conditions_of_Satisfaction__c', cast(Conditions_of_Satisfaction__c as nvarchar(max)) from SFCaseLand
-    where cast(Conditions_of_Satisfaction__c as nvarchar(max)) is not null and cast(Conditions_of_Satisfaction__c as nvarchar(max)) like '%</img>%'
-""")
-
-for row in cursor:
-    RTField_derived = {}
-    EID = row[0]
-    CaseNumber = row[1]
-    fieldname = row[2]
-    raw_text = row[3]
-    new_text = raw_text
-    # find html tags with Salesforce link
-    image_url_one = re.findall(
-        'https://c\.na79\.content\.force\.com/servlet/rtaImage\?eid=\S+&amp;feoid=\S+&amp;refid=\S+"', row[3])
-    #  find html tags with base64 encoded
-    image_url_two = re.findall('data:image/[png|gif]*;base64\S+"', row[3])
-    #  find html tags with local file path embedded
-    image_url_three = re.findall('src="file:///C:\S+', row[3])  # remove the tag from rax text
-
-    if image_url_one:
-        for each_url_one in image_url_one:
+def getsalesforceurlimage(raw_text):
+    image_url = re.findall(
+        'https://c\.na79\.content\.force\.com/servlet/rtaImage\?eid=\S+&amp;feoid=\S+&amp;refid=\S+"', raw_text)
+    if image_url:
+        for each_url in image_url:
             MD5Hash_dict = {}
             # Get each image in rich text field unique identifier
-            refid = each_url_one.split(';')[-1].replace('refid=', '').replace('"', '')
+            refid = each_url.split(';')[-1].replace('refid=', '').replace('"', '')
             url = "https://na79.salesforce.com/services/data/v43.0/sobjects/" + objectname + '/' + EID + \
                   '/richTextImageFields/' + fieldname + '/' + refid + '/'
+            headers = {'Authorization': 'Bearer ' + access_token}
             response = requests.get(url, headers=headers)
             #  Define the file type
             content_type = response.headers.get('Content-Type')
@@ -112,21 +72,26 @@ for row in cursor:
             #    html_file.close()
 
             #  replace <img> html tags into explanations text
-            new_text = re.sub(r'<img.+' + re.escape(each_url_one) + r'.+?</img>',
+            new_text = re.sub(r'<img.+' + re.escape(each_url) + r'.+?</img>',
                               '(Please refer to the following case note for screenshot - ' +
                               refid + file_type + ')', new_text)
-    if image_url_two:
+    return new_text, MD5Hash_list
+
+
+def getbase64encodedimage(new_text, md5hash_list):
+    image_url = re.findall('data:image/[png|gif]*;base64\S+"', row[3])
+    if image_url:
         i = 0
-        for each_url_two in image_url_two:
+        for each_url in image_url:
             MD5Hash_dict = {}
             i += 1  # file name identifier to differentiate multiple images in one field
             #  Get base64 encode
-            base64_code = each_url_two.replace('data:image/png;base64,', '').replace('data:image/gif;base64,', '') \
+            base64_code = each_url.replace('data:image/png;base64,', '').replace('data:image/gif;base64,', '') \
                 .replace('"', '').encode('utf-8')
             #  Define the file type
-            if 'data:image/png;base64,' in each_url_two:
+            if 'data:image/png;base64,' in each_url:
                 filename = CaseNumber + '_' + fieldname + '_' + str(i) + '.jpg'
-            elif 'data:image/gif;base64,' in each_url_two:
+            elif 'data:image/gif;base64,' in each_url:
                 filename = CaseNumber + '_' + fieldname + '_' + str(i) + '.gif'
             #  Generate the file path
             path = 'D:/App/Carbon/Salesforce/Image Attachments/' + env + '/Land/' + EID + '/'
@@ -152,8 +117,58 @@ for row in cursor:
             MD5Hash_dict['Name'] = filename
             MD5Hash_list.append(MD5Hash_dict)
             # replace <img> html tags into explanations text
-            new_text = re.sub(r'<img.+' + re.escape(each_url_two) + r'.+?</img>',
+            new_text = re.sub(r'<img.+' + re.escape(each_url) + r'.+?</img>',
                               '(Please refer to the following case note for screenshot - ' + filename + ')', new_text)
+    return new_text, MD5Hash_list
+
+
+access_token = getaccesstoken()
+
+#  Connect to Carbon database server
+conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                      "Server=;"
+                      "Database=CARBON" + env + ";"
+                      "Trusted_Connection=yes;")
+cursor = conn.cursor()  # connection to get all rich text fields with <img> tags
+lastrun = cursor.execute("select LastRun from Process_Control where ProcessName='SFRTField-Stage'")
+cursor.execute(r"""
+    select * from (select Id, CaseNumber, 'Problem_Recreation__c' as API_Name, cast(Problem_Recreation__c as nvarchar(max)) as RichText, LastModifiedDate from SFCaseLand 
+    where cast(Problem_Recreation__c as nvarchar(max)) is not null and cast(Problem_Recreation__c as nvarchar(max)) like '%</img>%'
+    union
+    select Id, CaseNumber, 'Root_Cause_FA__c', cast(Root_Cause_FA__c as nvarchar(max)), LastModifiedDate from SFCaseLand
+    where cast(Root_Cause_FA__c as nvarchar(max)) is not null and cast(Root_Cause_FA__c as nvarchar(max)) like '%</img>%'
+    union
+    select Id, CaseNumber, 'Most_Recent_Case_Comment__c', cast(Most_Recent_Case_Comment__c as nvarchar(max)), LastModifiedDate from SFCaseLand
+    where cast(Most_Recent_Case_Comment__c as nvarchar(max)) is not null and cast(Most_Recent_Case_Comment__c as nvarchar(max)) like '%</img>%'
+    union
+    select Id, CaseNumber, 'HubCase_Parnter_Comments__c', cast(HubCase_Parnter_Comments__c as nvarchar(max)), LastModifiedDate from SFCaseLand
+    where cast(HubCase_Parnter_Comments__c as nvarchar(max)) is not null and cast(HubCase_Parnter_Comments__c as nvarchar(max)) like '%</img>%'
+    union
+    select Id, CaseNumber, 'Current_Case_Status__c', cast(Current_Case_Status__c as nvarchar(max)), LastModifiedDate from SFCaseLand
+    where cast(Current_Case_Status__c as nvarchar(max)) is not null and cast(Current_Case_Status__c as nvarchar(max)) like '%</img>%'
+    union
+    select Id, CaseNumber, 'Conditions_of_Satisfaction__c', cast(Conditions_of_Satisfaction__c as nvarchar(max)), LastModifiedDate from SFCaseLand
+    where cast(Conditions_of_Satisfaction__c as nvarchar(max)) is not null and cast(Conditions_of_Satisfaction__c as nvarchar(max)) like '%</img>%') t
+    where t.LastModifiedDate >= ?
+""", lastrun)
+
+
+for row in cursor:
+    RTField_derived = {}
+    EID = row[0]
+    CaseNumber = row[1]
+    fieldname = row[2]
+    raw_text = row[3]
+    new_text = raw_text
+
+    # find html tags with Salesforce link
+    getsalesforceurlimage(raw_text)
+    #  find html tags with base64 encoded
+    getbase64encodedimage(new_text, MD5Hash_list)
+    #  find html tags with local file path embedded
+    image_url = re.findall('src="file:///C:\S+', row[3])  # remove the tag from rax text
+    if image_url:
+        pass
     #  Save the new text with its id and case number into a list of dictionary
     RTField_derived['CaseNumber'] = CaseNumber
     RTField_derived['Id'] = EID
@@ -161,16 +176,18 @@ for row in cursor:
     RTField_derived['Rich_Text_Field_derived'] = new_text
     RTField_list.append(RTField_derived)
 
-# Is there a good way to do upsert? I cannot figure it out so I directly use truncate and load instead...
-# insert id, case number and new text into SQL table
-cursor.execute("truncate table SFRTFieldStage")
+
+
+#  insert id, case number and new text into SQL table
+#  cursor.execute("truncate table SFRTFieldStage")
 for each_RTField in RTField_list:
-    #  print(each_RTField)
-    # cursor.execute("update SFRTFieldStage set CaseNumber=?, Id=?,APIName=?,Rich_Text_Field_derived=?",
-    #               (each_RTField['CaseNumber'], each_RTField['Id'], each_RTField['APIName'], each_RTField['Rich_Text_Field_derived']))
-    cursor.execute("insert into SFRTFieldStage (CaseNumber, Id, APIName, Rich_Text_Field_derived) values (?, ?, ?, ?)",
-                   (each_RTField['CaseNumber'], each_RTField['Id'], each_RTField['APIName'],
-                    each_RTField['Rich_Text_Field_derived']))
+    cursor.execute("select * from SFRTFieldStage where Id=? and APIName=?",(each_RTField['Id'], each_RTField['APIName']))
+    if cursor.rowcount == 0:
+        cursor.execute("insert into SFRTFieldStage (CaseNumber, Id, APIName, Rich_Text_Field_derived) values (?, ?, ?, ?)",
+            (each_RTField['CaseNumber'], each_RTField['Id'], each_RTField['APIName'], each_RTField['Rich_Text_Field_derived']))
+    else:
+        cursor.execute("update SFRTFieldStage set Rich_Text_Field_derived=? where Id=? and APIName=?",
+                       (each_RTField['Rich_Text_Field_derived'], each_RTField['Id'], each_RTField['APIName']))
     cursor.commit()
 
 # insert MD5 hash, Case Id and File name into SFImageAttachmentLand table
